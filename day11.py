d6e2bd18943a2d07148a9243b252614181821918
prajudhamane8010 <pranjaldhamane7@gmail.com>
2026-07-20 13:53:34 +0530

    Add script to reverse a string

---
+++ day11.py
@@ -0,0 +1,12 @@
+# Day 11 - Reverse a String
+# Date: 2026-07-20
+
+print("==== Day 11 : Reverse a String ====\n")
+
+text = input("Enter a string: ")
+
+reverse = text[::-1]
+
+print("\nReversed String:", reverse)
+
+print("\nProgram Completed Successfully!")
